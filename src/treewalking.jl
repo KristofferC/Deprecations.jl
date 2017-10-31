4056afb3b647d421aa9cec0ae23c29bf2dc9a154
Kristoffer Carlsson <kcarlsson89@gmail.com>
2017-10-31 13:18:25 +0100

    update treewalking (needs cstparser backport)

--- src/treewalking.jl
+++ src/treewalking.jl
@@ -10,20 +10,23 @@ struct OverlayNode{T}
     fullspan::UnitRange{Int}
     span::UnitRange{Int}
 end
+
 Base.Expr(n::OverlayNode) = Base.Expr(n.expr)
 AbstractTrees.parentlinks(::Type{<:OverlayNode}) = AbstractTrees.StoredParents()
 AbstractTrees.siblinglinks(::Type{<:OverlayNode}) = AbstractTrees.ImplicitSiblings()
 AbstractTrees.parent(node::OverlayNode) = node.parent
 AbstractTrees.isroot(node::OverlayNode) = node.parent == nothing
-OverlayNode(expr, buffer) = OverlayNode(nothing, buffer, expr, 0:(expr.fullspan-1), expr.span - 1)
 
-AbstractTrees.printnode(io::IO, o::OverlayNode{T}) where {T} = print(io, T, "  ", o.fullspan, " (", o.span, ")")
+OverlayNode(expr, buffer) = OverlayNode(nothing, buffer, expr, 0:(expr.fullspan-1), expr.span - 1)
+AbstractTrees.printnode(io::IO, o::OverlayNode{T}) where {T} = print(io, sprint(AbstractTrees.printnode, o.expr), " -- ", o.fullspan, " (", o.span, ")")
 Base.show(io::IO, o::OverlayNode) = AbstractTrees.print_tree(io, o)
 
 Base.length(o::OverlayNode) = length(o.expr.args)
-Base.endof(o::OverlayNode) = endof(o.expr.args)
+Base.endof(o::OverlayNode)  = endof(o.expr.args)
 
 children(node::OverlayNode) = node
+
+
 function Base.getindex(node::OverlayNode, idx::Integer)
     offset = first(node.fullspan)
     for i = 1:idx-1
@@ -36,15 +39,18 @@ function Base.getindex(node::OverlayNode, range::Range)
     map(x->node[x], range)
 end
 
-Base.setindex!(o::OverlayNode, x::OverlayNode, idx) = Base.setindex!(o.expr.args, x.expr, idx)
+function Base.setindex!(o::OverlayNode, x::OverlayNode, idx)
+    Base.setindex!(o.expr.args, x.expr, idx)
+end
 
-Base.start(node::OverlayNode) = (1, first(node.fullspan))
+
+Base.start(node::OverlayNode) = (start(node.expr), first(node.fullspan))
 function Base.next(node::OverlayNode, state::Tuple{Int, Int})
     idx, offset = state
-    expr = node.expr.args[idx]
-    (OverlayNode(node, node.buffer, expr, offset:(offset+expr.fullspan-1), offset - 1 + expr.span), (idx+1, offset + expr.fullspan))
+    expr, idx = next(node.expr, idx)
+    (OverlayNode(node, node.buffer, expr, offset:(offset+expr.fullspan-1), offset - 1 + expr.span), (idx, offset + expr.fullspan))
 end
-Base.done(node::OverlayNode, state::Tuple{Int, Int}) = state[1] > length(node.expr.args)
+Base.done(node::OverlayNode, state::Tuple{Int, Int}) = done(node.expr, state[1])
 
 function function_def_call(expr)
     if isexpr(expr, FunctionDef)
